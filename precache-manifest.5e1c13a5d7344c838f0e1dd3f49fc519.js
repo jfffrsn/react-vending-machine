e4266dc8bdc8acbff7cee1df7113ab6f8bddfd4f
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2020-01-20 00:58:25 -0500

    Updates

---
+++ precache-manifest.5e1c13a5d7344c838f0e1dd3f49fc519.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a2bf4ed8932ba150c482d2825c521c",
+    "url": "/react-vending-machine/index.html"
+  },
+  {
+    "revision": "bc592586476767b27972",
+    "url": "/react-vending-machine/static/css/main.3ac87c6f.chunk.css"
+  },
+  {
+    "revision": "f4be768e8482da830c1a",
+    "url": "/react-vending-machine/static/js/2.57a2e871.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-vending-machine/static/js/2.57a2e871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc592586476767b27972",
+    "url": "/react-vending-machine/static/js/main.8602387a.chunk.js"
+  },
+  {
+    "revision": "2a7bc0e5726f4b9ed5e0",
+    "url": "/react-vending-machine/static/js/runtime-main.8f0fd0d7.js"
+  },
+  {
+    "revision": "0e21446e438c1aeb2913a4c95776436a",
+    "url": "/react-vending-machine/static/media/Chips.0e21446e.png"
+  },
+  {
+    "revision": "7746d7d5d94a644ffd6cdb1876f72a87",
+    "url": "/react-vending-machine/static/media/Soda.7746d7d5.png"
+  },
+  {
+    "revision": "369bea35cf50c57aa4cb31df6c8f4da3",
+    "url": "/react-vending-machine/static/media/VendingMachine.369bea35.png"
+  }
+]);
